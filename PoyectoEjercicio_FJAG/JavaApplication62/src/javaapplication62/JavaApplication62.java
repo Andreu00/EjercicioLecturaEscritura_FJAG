e4c32f2a1880c39355d5ccbbaef68aa296af55f6
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2023-12-04 19:31:31 +0100

    add comments

--- PoyectoEjercicio_FJAG/JavaApplication62/src/javaapplication62/JavaApplication62.java
+++ PoyectoEjercicio_FJAG/JavaApplication62/src/javaapplication62/JavaApplication62.java
@@ -17,14 +17,21 @@ public class JavaApplication62 {
         BufferedWriter escritura=null;
         FileWriter nuevoArchivo=null;
         try{
+            //Aqui declararemos el nombre del fichero txt del que leeremos su contenido
             entrada = new FileInputStream("alumnosNotas.txt");
+            //En esta linea leeremos el fichero txt
             lectura=new BufferedReader(new InputStreamReader(entrada));
+            //Declaramos el archivo txt en el que almacenaremos el contenido de las medias.
+            //True nos ayudará para no sobreescribir el contenido de este cada vez que ejecutemos
             nuevoArchivo=new FileWriter("media.txt", true);
+            //Declaramos el BufferedWiter para indicar donde almacenaremos el contenido utilizando la variable creada para ello
             escritura=new BufferedWriter(nuevoArchivo);
             String linea;
+            //Creamos un String linea para utilizarlo en este bucle para comprobar cual será el final del fichero txt
             while ((linea = lectura.readLine()) != null) {
-                String[] partes = linea.split(":");
+                String[] partes = linea.split(":"); //El split lo que hace es, con el valor que le indicamos, marcar la separacion de la cual cogerá los elementos
                 String nombre = partes[0];
+                //Pasamos las notas a int para trabajar con ellas
                 int nota1 = Integer.parseInt(partes[1]);
                 int nota2 = Integer.parseInt(partes[2]);
                 int nota3 = Integer.parseInt(partes[3]);
@@ -33,8 +40,8 @@ public class JavaApplication62 {
                 
                 
                 String mensaje="Nombre: " + nombre + ", Promedio: " + promedio+"\n";
+                //Escribimos en el fichero en el que vamos a almacenar la nota media de cada uno
                 escritura.write(mensaje);
-                //System.out.println("Nombre: " + nombre + ", Promedio: " + promedio);
             }
         }catch(IOException e){
             System.out.println(e);
